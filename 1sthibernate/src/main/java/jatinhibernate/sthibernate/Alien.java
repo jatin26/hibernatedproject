9bcb8c8d29a153165e048300af9e9c605161f043
jatin <jatin@192.168.0.10>
2019-04-08 23:43:41 +0530

    Hibernate CRUD Application project

--- 1sthibernate/src/main/java/jatinhibernate/sthibernate/Alien.java
+++ 1sthibernate/src/main/java/jatinhibernate/sthibernate/Alien.java
@@ -1,15 +1,20 @@
 package jatinhibernate.sthibernate;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
-
+//@Table(name="alien_new")//it is used to add new table in a database
 public class Alien {
 
 	@Id
 	private int aid;
+	//@Transient//it is used to no data store in a sqecific column
 	private String aname;
+	//@Column(name="diff_color")//it is used to add new column and replace old column value to new column value 
 	private String color;
 	public int getAid() {
 		
@@ -31,6 +36,10 @@ public class Alien {
 	}
 	public void setColor(String color) {
 		this.color = color;
+	}
+	@Override
+	public String toString() {
+		return "Alien [aid=" + aid + ", aname=" + aname + ", color=" + color + "]";
 	}
 	
 	
--- 1sthibernate/src/main/java/jatinhibernate/sthibernate/App.java
+++ 1sthibernate/src/main/java/jatinhibernate/sthibernate/App.java
@@ -1,30 +1,74 @@
 package jatinhibernate.sthibernate;
 
+import javax.transaction.Synchronization;
+
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.engine.transaction.spi.LocalStatus;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-       Alien jatin=new Alien();
-       
-       jatin.setAid(2);
-       jatin.setAname("jatin");
-       jatin.setColor("green");
-       
-       Configuration con=new Configuration().configure().addAnnotatedClass(Alien.class);
-       SessionFactory sf=con.buildSessionFactory();
-       Session session=sf.openSession();
-       Transaction tx=session.beginTransaction();
-       session.save(jatin);
-       
-       tx.commit();
+
+    	//*It is used to insert data in database*// 
+    	
+    	//       Alien jatin=new Alien();
+//       
+//       jatin.setAid(8);
+//       jatin.setAname("amit");
+//       jatin.setColor("white");
+//       
+//       Configuration con=new Configuration().configure().addAnnotatedClass(Alien.class);
+//       ServiceRegistry reg=new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
+//       SessionFactory sf=con.buildSessionFactory(reg);
+//       Session session=sf.openSession();
+//       Transaction tx=session.beginTransaction();
+//       session.save(jatin);
+//       
+//       tx.commit();
+    	
+    	//*It is used to fetch the data in the database*//
+
+    	//    	Alien jatin=new Alien();
+//    	Configuration con=new Configuration().configure().addAnnotatedClass(Alien.class);
+//    	ServiceRegistry reg=new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
+//    	SessionFactory sf=con.buildSessionFactory(reg);
+//    	Session session=sf.openSession();
+//    	Transaction tx=session.beginTransaction();
+//    	jatin=(Alien)session.get(Alien.class, 2);
+//    	tx.commit();
+//    	System.out.println(jatin);
+    	
+    	//*It is used to Update the data in the database*
+    	
+//    	Alien jatin=new Alien();
+//    	Configuration con=new Configuration().configure().addAnnotatedClass(Alien.class);
+//    	ServiceRegistry reg=new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
+//    	SessionFactory sf=con.buildSessionFactory(reg);
+//    	Session session=sf.openSession();
+//    	Transaction tx=session.beginTransaction();
+//    	jatin=(Alien)session.get(Alien.class, 5);
+//    	jatin.setAname("sahil");
+//    	session.update(jatin);
+//    	tx.commit();	
+   
+    	//*It is used to delete the data into the database*
+
+    	//    	Alien jatin=new Alien();
+//    	Configuration con=new Configuration().configure().addAnnotatedClass(Alien.class);
+//    	ServiceRegistry reg=new ServiceRegistryBuilder().applySettings(con.getProperties()).buildServiceRegistry();
+//    	SessionFactory sf=con.buildSessionFactory(reg);
+//    	Session session=sf.openSession();
+//    	Transaction tx=session.beginTransaction();
+//    	jatin=(Alien)session.get(Alien.class, 7);
+//    	jatin.setAname("");
+//    	session.delete(jatin);
+//    	tx.commit();
     }
 }
